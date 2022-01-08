243e4c1e0d52296c82ed483069d635a8947068cb
nicolasbahindwa@gmail.com <nicolasbahindwa@gmail.com>
2022-01-08 10:24:45 +0900

    basic set up, user class override , firstmigration

---
+++ apps/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import UserViewSet
+
+
+router = routers.DefaultRouter()
+router.register('', UserViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls))
+
+]
